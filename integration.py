d0452d3e7f442a221e24361ecc799420b44753ef
Luan Andrade <luansandrade92@hotmail.com>
2016-07-11 16:49:08 -0300

    test integrate method and exceptions

--- integration.py
+++ integration.py
@@ -1,11 +1,10 @@
 
 class Integration:
-
-	def integrate(self):
-		return 1
-
+	orders = []
 
 	def set_input(self,input):
+		if input == None or input == []:
+			raise Exception('You must pass a valid array')
 		self.input = input
 
 	def get_size_input(self):
@@ -16,3 +15,50 @@ class Integration:
 			return True
 		else:
 			return False
+
+	def get_freight(self):
+		return self.input[0]
+
+	def get_itens_prices(self):
+		return self.input[1]
+
+	def get_totals(self):
+		return self.input[2]
+
+
+	def integrate(self,t_index,solutions):
+		totals = sorted(self.get_totals())
+		if t_index==len(totals):
+			return solutions
+
+		freights = self.get_freight()
+		itens = self.get_itens_prices()
+
+		for i_index in range(len(self.get_itens_prices())):
+			for f_index in range(len(self.get_freight())):
+				if ([f_index,i_index] in solutions):
+					break
+
+				if self.match(freights[f_index],itens[i_index],totals[t_index]):
+					return self.integrate(t_index+1,solutions+[[f_index,i_index]])
+
+	def match(self,f,i,t):
+		if (f+i)==t:
+			return True
+		else:
+			return False
+
+	def set_orders(self,orders):
+		totals = sorted(self.get_totals())
+		freights = self.get_freight()
+		itens = self.get_itens_prices()
+		i = 0
+		for order in orders:
+			self.orders.append([freights[order[0]],itens[order[1]],totals[i]])
+			i+=1
+		
+	def get_orders(self):
+		return self.orders
+
+		
+		
--- test.py
+++ test.py
@@ -10,16 +10,48 @@ class IntegrationTest(unittest.TestCase):
 
 	def test_size_input(self):
 		self.i.set_input(self.input)
-
 		self.assertEqual(3,self.i.get_size_input(),"Your input must contain 3 arrays.")
 
 	def test_homogeneously_input(self):
 		self.i.set_input(self.input)
-
 		self.assertTrue(self.i.is_input_homogeneous(),"Arrays must have the same size.")
+
 		wrong_input = [[3,2,1],[1,2],[1]]
 		self.i.set_input(wrong_input)
 		self.assertFalse(self.i.is_input_homogeneous(),"Arrays must have the same size.")
+
+	def test_get_freight(self):
+		self.i.set_input(self.input)
+		self.assertEqual(self.input[0],self.i.get_freight(),"Not the same array of freight.")
+
+	def test_get_itens_prices(self):
+		self.i.set_input(self.input)
+		self.assertEqual(self.input[1],self.i.get_itens_prices(),"Not the same array of itens.")
+
+	def test_get_totals(self):
+		self.i.set_input(self.input)
+		self.assertEqual(self.input[2],self.i.get_totals(),"Not the same array of totals.")
+
+	def test_match(self):
+		self.assertTrue(self.i.match(1,1,2),"Doesn't match.")
+		self.assertFalse(self.i.match(1,1,3),"Doesn't match.")
+
+
+	def test_integrate(self):
+		expected = [[25,25,50],[20,40,60],[40,30,70],[50,50,100],[45,60,105]]
+		self.i.set_input(self.input)
+		result = self.i.integrate(0,[])
+		self.i.set_orders(result)
+		self.assertEqual(expected,self.i.get_orders(),"Result doesn't match")		
+
+	# testar array nulo ou vazio (exception)
+	def test_null_array(self):
+		self.assertRaises(Exception, self.i.set_input,None)
+
+	def test_empty_array(self):
+		self.assertRaises(Exception, self.i.set_input,[])
+
+	# testar para todos os tamanhos de arrays
 
 
 if __name__ == '__main__':
