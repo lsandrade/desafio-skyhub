4d4f71b62f3c613d36d441ffa544c0dd5cb6c8b4
Luan Andrade <luansandrade92@hotmail.com>
2016-07-11 10:36:38 -0300

    test input

--- integration.py
+++ integration.py
@@ -1,4 +1,18 @@
 
 class Integration:
+
 	def integrate(self):
 		return 1
+
+
+	def set_input(self,input):
+		self.input = input
+
+	def get_size_input(self):
+		return len(self.input)
+
+	def is_input_homogeneous(self):
+		if len(self.input[0]) == len(self.input[1]) and len(self.input[1])==len(self.input[2]):
+			return True
+		else:
+			return False
--- test.py
+++ test.py
@@ -8,7 +8,18 @@ class IntegrationTest(unittest.TestCase):
 	def test_instance_of_integration(self):
 		self.assertIsInstance(self.i,Integration,"Not a Integration.")
 
-	
+	def test_size_input(self):
+		self.i.set_input(self.input)
+
+		self.assertEqual(3,self.i.get_size_input(),"Your input must contain 3 arrays.")
+
+	def test_homogeneously_input(self):
+		self.i.set_input(self.input)
+
+		self.assertTrue(self.i.is_input_homogeneous(),"Arrays must have the same size.")
+		wrong_input = [[3,2,1],[1,2],[1]]
+		self.i.set_input(wrong_input)
+		self.assertFalse(self.i.is_input_homogeneous(),"Arrays must have the same size.")
 
 
 if __name__ == '__main__':
